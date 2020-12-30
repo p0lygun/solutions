feb4dc0b3d145a1c0129edd0cf1ab59de2e12b0c
Vibhakar Solanki <solankivibhakar82@gmail.com>
2020-12-30 14:08:55 +0530

    Create n0_idea.py

---
+++ hackerrank/practice/python/n0_idea.py
@@ -0,0 +1,5 @@
+n,m = map(int,input().split())
+my = list(map(int,input().split()))
+a = set(map(int,input().split()))
+b = set(map(int,input().split()))
+print(sum([ 1 if i in a else -1 if i in b else 0 for i in my]))
