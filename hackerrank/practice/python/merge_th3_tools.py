9ebfd12c97c0923b6e5c75b432a938627168c4e7
Vibhakar Solanki <solankivibhakar82@gmail.com>
2020-12-30 13:14:38 +0530

    Update merge_th3_tools.py

--- hackerrank/practice/python/merge_th3_tools.py
+++ hackerrank/practice/python/merge_th3_tools.py
@@ -1,8 +1,8 @@
 ##pretty easy
 def merge_the_tools(string, k):
-    subs = [string[i:i+k] for i in range(0,len(string),k)]
+    subs = [string[i:i+k] for i in range(0,len(string),k)] #splits at every nth char
     for elm in subs:
-        print(''.join(dict().fromkeys(elm).keys()))
+        print(''.join(dict().fromkeys(elm).keys())) #dict only has "unique" keys
 
 if __name__ == '__main__':
     string, k = input(), int(input())
